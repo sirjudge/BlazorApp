168405e2cdfb7f18491b43990217ec7ca071dd7b
nico <nico.a.judge@gmail.com>
2023-06-07 17:27:00 -0400

    redid blazor structure from scracth and added some testing buttons

--- blazorTestAPp/Program.cs
+++
@@ -1,9 +0,0 @@
-using blazorTestAPp;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/hello", () => "Hello but it's mapped to a pattern");
-
-app.Run();
