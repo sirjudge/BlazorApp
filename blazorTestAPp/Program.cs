4d3052882118626d29aaba47c29740974c961dc7
Nico <=>
2023-06-07 13:44:37 -0400

    added gitignore

--- blazorTestAPp/Program.cs
+++ blazorTestAPp/Program.cs
@@ -4,4 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => "Hello but it's mapped to a pattern");
+
 app.Run();
